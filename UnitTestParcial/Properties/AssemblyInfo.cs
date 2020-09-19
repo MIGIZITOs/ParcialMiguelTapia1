ef86f7034358f87b760953b000b4acc7bf60c3bc
Miguel Tapia Montecinos <a2019118017@estudiantes.upsa.edu.bo>
2020-09-19 12:30:16 -0400

    Commit UNITTEST CREADO

---
+++ UnitTestParcial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestParcial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestParcial")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("601b99ef-e79c-4351-9cc8-6c8fdb313b98")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestParcial/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialMiguelTapia1.Controllers;
+
+namespace UnitTestParcial
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGet()
+        {   //ARRANGE
+            CountriesController controller = new CountriesController();
+            //ACT
+            var lista = controller.GetCountries();
+            //assert
+            Assert.IsNotNull(lista);
+        }
+    }
+}
